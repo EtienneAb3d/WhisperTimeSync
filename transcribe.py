de718b054f16f0a46d19b917d71990dd48a060ec
cubAIx <contact@cubaix.com>
2023-02-10 08:29:39 +0100

    Proper file path

--- transcribe.py
+++ transcribe.py
@@ -1,4 +1,5 @@
 import sys
+from pathlib import Path
 import whisper
 from whisper.utils import WriteSRT
 
@@ -6,10 +7,11 @@ modelSize = "large"
 
 if __name__ == "__main__":
     mp3 = sys.argv[1] #"data/KatyPerry-Firework.mp3"
+    
     if len(sys.argv) >= 2 :
         modelSize = sys.argv[2]
     
-    print("Loading Whisper model...")
+    print("Loading Whisper model: "+modelSize+" ...")
     model = whisper.load_model(modelSize)
     
     print("Transcribing: "+mp3+" ...")
@@ -18,6 +20,7 @@ if __name__ == "__main__":
     print(result["text"])
     
     print("Saving: "+mp3+".srt ...")
-    writer = WriteSRT("data/")
+    p = Path(mp3)
+    writer = WriteSRT(p.parent)
     writer(result, mp3)
 
